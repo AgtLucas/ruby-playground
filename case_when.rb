c2886b932aad1c2e8dae594a5e3489008ad86c0d
Lucas <metallucas.ds@gmail.com>
2013-05-07 21:49:51 -0300

    changes

--- case_when.rb
+++ case_when.rb
@@ -3,10 +3,11 @@ car_model = gets.strip
 
 # Process
 output = case car_model
-	when "P1" then "McLaren"
+	when "P1", "F1" then "McLaren"
 	when "Focus" then "Ford"
 	when "Civic" then "Honda"
 	else "Unknown model"
+	end
 
 # Output
 puts "The car company for #{car_model} is ", output
