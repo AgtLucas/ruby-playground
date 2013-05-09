09ad9694abc9cb7ba96e39420c06d138126d9ea7
Lucas <metallucas.ds@gmail.com>
2013-05-08 21:49:37 -0300

    better way to loop

--- loops.rb
+++ loops.rb
@@ -1,5 +1,9 @@
-number = 0
-until number > 15 do
+# number = 0
+# until number > 15 do
+# 	puts "The number inside the loop is #{number}"
+# 	number += 1
+# end
+
+16.times do |number|
 	puts "The number inside the loop is #{number}"
-	number += 1
 end
