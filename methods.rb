c86b499e3e0fdb531a3ef9863f11f4dbc303bc45
Lucas <metallucas.ds@gmail.com>
2013-08-04 10:20:52 -0300

    Updates methods

--- methods.rb
+++ methods.rb
@@ -5,8 +5,11 @@
 class Post
   attr_reader :author, :title, :body, :comments
 
-  def initialize author, title, body, comments
-
+  def initialize author, title, body, comments = []
+    @author   = author
+    @title    = title
+    @body     = body
+    @comments = comments
   end
 end
 
@@ -20,4 +23,6 @@ end
 
 # Program
 
-post = Post.new "Lucas Churchill", "My first post", "The post...", []
+post = Post.new "Lucas Churchill", "My first post", "The post..."
+
+p post.inspect
