766003d693167e4857bad14f70b4aa78d731dded
Lucas <metallucas.ds@gmail.com>
2013-08-04 10:13:04 -0300

    Methods

--- methods.rb
+++ methods.rb
@@ -4,8 +4,16 @@
 
 class Post
   attr_reader :author, :title, :body, :comments
+
+  def initialize author, title, body, comments
+
+  end
 end
 
 class Comment
   attr_reader :user, :body
+
+  def initialize user, body
+
+  end
 end
