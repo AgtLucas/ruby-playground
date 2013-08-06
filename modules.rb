ccbf4aaa5647b887c3dd816cd802e6e26918d55b
Lucas <metallucas.ds@gmail.com>
2013-08-06 17:26:07 -0300

    Update modules

--- modules.rb
+++ modules.rb
@@ -1,1 +1,8 @@
 require_relative "blog"
+
+post = Blog::Post.new author: "Lucas Churchill",
+                      title: "A title",
+                      body: "The body",
+                      comments: Blog::Comment.new(user: "Logan", body: "A comment")
+
+p post
