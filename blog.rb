6a623620614c40a63b5f8eef27f8909ed5c32dd9
Lucas <metallucas.ds@gmail.com>
2013-08-06 17:19:15 -0300

    Add blog file

---
+++ blog.rb
@@ -0,0 +1,26 @@
+module Blog
+class Post
+  attr_reader :author, :title, :body, :comments
+
+  def initialize options
+    @author   = options[:author]
+    @title    = options[:title]
+    @body     = options[:body]
+    @comments = options[:comments] || []
+  end
+
+  def insert_comment first, second, *thirds, options, &block
+    comments.each { |c| @comments << c }
+  end
+end
+
+class Comment
+  attr_reader :user, :body
+
+  def initialize options
+    @user = options[:user]
+    @body = options[:body]
+  end
+end
+
+end
