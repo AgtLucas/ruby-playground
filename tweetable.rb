d5c37d7cbe9ddb91c942d2df9d4b5e8c44fb87b8
Lucas <metallucas.ds@gmail.com>
2013-08-11 15:35:39 -0300

    Update tweetable

--- tweetable.rb
+++ tweetable.rb
@@ -1,7 +1,7 @@
 module Tweetable
   class NoBodyError < StandardError; end
   def tweet
-    raise noBodyError if @body.nil?
+    raise NoBodyError if @body.nil?
     puts "Tweeted the following: #{@body}"
   end
 end
